aea91e844f86211e4692b3a340e65e7b46e07164
AbinashC2 <abinashmohapatra0007@gmail.com>
2024-09-03 18:00:20 +0530

    async & await

--- server.js
+++ server.js
@@ -12,23 +12,23 @@ app.get('/', function (req, res) {
 })
 
 // POST route to add a person
-app.post('/person', (req, res)=>{
+app.post('/person', async (req, res)=>{
     
-  const data =req.body // asumming the request body contains the person data
-  
-  //create a new person document using monoose model
-  const newPerson = newPerson(data);
-
-  //save the new person to the database
-  newPerson.save((error, savedPerson)=>{
-    if(error){
-      console.log('error saving person:' , error);
-      res.status(500).json({error: 'Internal server error'})
-    }else{
-      console.log('data saved successfully');
-      res.status(200).json(savedPerson);
-    }
-  })
+  try {
+          const data =req.body // asumming the request body contains the person data
+        
+          //create a new person document using monoose model
+          const newPerson = new Person(data);
+        
+          //save the new person to the database
+        const response = await newPerson.save();
+        console.log('data saved');
+        res.status(200).json(response);
+  } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server Error'});
+  }
+ 
 })
 
 
