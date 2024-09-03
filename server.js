c056817db4b1ee0039c23ce6c5aafe3d0d9fb905
AbinashC2 <abinashmohapatra0007@gmail.com>
2024-09-03 11:15:05 +0530

    server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const db = require('./db');
 const app = express();
 
-
+const Person = require('./models/Person');
+ 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
