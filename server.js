950bf11b652ea7d991be0d983875dfab5cadb068
AbinashC2 <abinashmohapatra0007@gmail.com>
2024-09-03 12:16:02 +0530

    added post api for person data save

--- server.js
+++ server.js
@@ -2,10 +2,33 @@ const express = require('express');
 const db = require('./db');
 const app = express();
 
+const bodyParser = require ('body-parser');
+app.use(bodyParser.json()); // req body
+
 const Person = require('./models/Person');
  
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+// POST route to add a person
+app.post('/person', (req, res)=>{
+    
+  const data =req.body // asumming the request body contains the person data
+  
+  //create a new person document using monoose model
+  const newPerson = newPerson(data);
+
+  //save the new person to the database
+  newPerson.save((error, savedPerson)=>{
+    if(error){
+      console.log('error saving person:' , error);
+      res.status(500).json({error: 'Internal server error'})
+    }else{
+      console.log('data saved successfully');
+      res.status(200).json(savedPerson);
+    }
+  })
 })
 
 
